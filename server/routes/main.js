c1d74d6f14a27a00d6c8bfa931f8a58b763aaf8f
giannasaurus <gianzcantor@gmail.com>
2025-11-14 11:33:21 +0800

    2 days of work +> Mongoose model (expand)
    changes, not in chronological order:
    - invoke connectDB() in app.js
    - create db connection
    - create mongoose schema and export as model
    - find Post model and render it with index.ejs and locals object
    - modify about.ejs and contact.ejs
    - modify index.ejs template to recursively generate blog posts (title + date)
    - change main tag to div tag in main.ejs
    - add dynamic year in footer.ejs
    - remove whitespaces in header.ejs

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,21 +1,53 @@
 const express = require('express')
 const router = express.Router()
+const Post = require('../models/Post')
 
-// Routes
-router.get('/', (req, res) => {
+// GET / HOME
+router.get('/', async (req, res) => {
     const locals = {
-        title: "Nodejs Blog",
+        title: "NodeJS Blog",
         description: "Simple blog created with Nodejs, Express, and MongoDB"
     }
-    res.render('index', { locals })
+    
+    try {
+        const data = await Post.find()
+        res.render('index', { locals, data })
+    }
+    catch (error) {
+        console.error(error)
+    }
 })
 
 router.get('/about', (req, res) => {
     const locals = {
-        title: "About | Nodejs Blog",
+        title: "About | NodeJS Blog",
         description: "About this Nodejs blog"
     }
     res.render('about', { locals })
 })
+
+router.get('/contact', (req, res) => {
+    const locals = {
+        title: "Contact | NodeJS Blog",
+        description: "Contact the creator of this"
+    }
+    res.render('contact', { locals })
+})
+
+// function insertPostData() {
+//     try {
+//         Post.insertMany([
+//             {
+//                 title: "Building a blog",
+//                 body: "This is the body text"
+//             }
+//         ])
+//         console.log('Post data inserted')
+//     }
+//     catch (err) {
+//         console.error('Insert error:', err)
+//     }
+// }
+// insertPostData()
 
 module.exports = router
